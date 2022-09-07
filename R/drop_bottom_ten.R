6302d97eb2fe6e5854b8590f1d1ba72ae106a8f6
amvhihg <axv851@miami.edu>
2022-09-07 17:55:17 -0400

    added vsd_df

--- R/drop_bottom_ten.R
+++ R/drop_bottom_ten.R
@@ -1,4 +1,5 @@
 drop_bottom_ten <- function(vsd_mat){
+  vsd_df <- as.data.frame(vsd_mat)
   vsd_df$var <- apply(vsd_mat,1,var)
   quant <- quantile(vsd_df$var, probs = 0.10)
   vsd_df <- vsd_df[vsd_df$var > quant,]
